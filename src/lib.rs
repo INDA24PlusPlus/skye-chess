fa3348663eb87dafc74d1b83c0e229528570da2d
Skye Kaijser <skye@kth.se>
2024-09-17 11:26:43 +0200

    Added moves for knight and king. Created a check for long castling with white, deciding if you are currently in check, getting a capture map, queriyng for a piece in a position. TODO add tests

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,34 @@ struct ChessBoard {
     pieces: [ChessPiece;32]
 }
 
+fn get_rank(piece: ChessPiece)->u8{
+    if piece.is_captured {
+        return 0;
+    }
+    let mut scanner:u64=0xFF;
+    for rank in 1..=8 {
+        if(scanner & piece.pos)>0 {
+            return 9-rank;
+        }
+        scanner=scanner<<8;
+    }
+    return 0;
+}
+
+fn get_file(piece: ChessPiece)->u8{
+    if piece.is_captured {
+            return 0;
+    }
+    let mut scanner = 0x8080808080808080;
+    for file in 1..=8 {
+        if(scanner & piece.pos)>0 {
+            return file;
+        }
+        scanner=scanner>>1;
+    }
+    return 0;
+}
+
 fn get_piece_map(col:ChessColour, board:ChessBoard)->u64{
     let mut out:u64=0x00;
     for piece in board.pieces{
@@ -51,6 +79,14 @@ fn get_op_col(col:ChessColour)->ChessColour{
         ChessColour::Black=>ChessColour::White,
     };
 }
+fn get_piece_bit_mask(pos:u64, board:ChessBoard)->Option<ChessPiece>{
+    for piece in board.pieces{
+        if (piece.pos&pos)>0{
+            return Some(piece);
+        }
+    }
+    return None;
+}
 fn get_ep_capture_spots(col:ChessColour, board:ChessBoard)->u64{
     let mut out:u64=0x00;
     for piece in board.pieces{
@@ -68,8 +104,10 @@ fn get_ep_capture_spots(col:ChessColour, board:ChessBoard)->u64{
         }
     }
     return out;
+
 }
-fn pawn_get_moves(piece: ChessPiece,board:ChessBoard)->u64{
+
+fn get_pawn_moves(piece: ChessPiece,board:ChessBoard)->u64{
     let mut out:u64=0x0;
     
     if piece.colour==ChessColour::White{
@@ -88,35 +126,112 @@ fn pawn_get_moves(piece: ChessPiece,board:ChessBoard)->u64{
     }
     return out;
 }
-fn pawn_get_captures(piece: ChessPiece, board: ChessBoard){
-    if piece.colour
-}
-fn get_rank(piece: ChessPiece)->u8{
-    if piece.is_captured {
-        return 0;
+
+fn get_pawn_captures(piece: ChessPiece, _board:ChessBoard)->u64{
+    if piece.colour==ChessColour::White{
+        return (piece.pos<<7)|(piece.pos<<9);
+    } 
+    else if piece.colour==ChessColour::Black {
+        return (piece.pos>>7)|(piece.pos>>9);
     }
-    let mut scanner:u64=0xFF;
-    for rank in 1..=8 {
-        if(scanner & piece.pos)>0 {
-            return 9-rank;
+    return 0x0;
+}
+
+fn get_knight_moves(piece: ChessPiece, board:ChessBoard)->u64{
+    return ((piece.pos>>6)|(piece.pos<<6)|(piece.pos>>10)|(piece.pos<<10)|(piece.pos>>15)|(piece.pos<<15)|(piece.pos>>17)|(piece.pos<<17))&(!get_piece_map(piece.colour,board));
+}
+fn get_rook_moves(piece: ChessPiece, board:ChessBoard)->u64{
+    todo!("Implement rook movement");
+}
+fn get_bishop_moves(piece:ChessPiece, board:ChessBoard)->u64{
+    todo!("Implement bishop movement");
+}
+fn get_queen_moves(piece:ChessPiece, board:ChessBoard)->u64{
+    return get_bishop_moves(piece,board)|get_rook_moves(piece, board);
+}
+fn get_king_moves_colour(col:ChessColour, board:ChessBoard)->u64{
+    for piece in board.pieces{
+        if piece.colour!=col{continue;}
+        if piece.kind!=ChessPieceKind::King{continue;}
+        return ((piece.pos>>1)|(piece.pos>>9)|(piece.pos>>8)|(piece.pos>>7)|(piece.pos<<1)|(piece.pos<<9)|(piece.pos<<8)|(piece.pos<<7))&(!(get_piece_map(col,board)));
+
+    }
+    return 0x00;
+}
+
+fn get_capture_map_king_check(col:ChessColour,board:ChessBoard)->u64{
+    let mut out:u64=0x00;
+    for piece in board.pieces{
+        if piece.colour!=col{continue;}
+        out=out|match piece.kind{
+            ChessPieceKind::Rook=>get_rook_moves(piece, board),
+            ChessPieceKind::Pawn=>get_pawn_captures(piece, board),
+            ChessPieceKind::Knight=>get_knight_moves(piece, board),
+            ChessPieceKind::King=>get_king_moves_colour(col,board),
+            ChessPieceKind::Bishop=>get_bishop_moves(piece, board),
+            ChessPieceKind::Queen=>get_queen_moves(piece, board),
         }
-        scanner=scanner<<8;
     }
-    return 0;
+    return out;
 }
-fn get_file(piece: ChessPiece)->u8{
-    if piece.is_captured {
-            return 0;
-    }
-    let mut scanner = 0x8080808080808080;
-    for file in 1..=8 {
-        if(scanner & piece.pos)>0 {
-            return file;
+
+fn get_king_moves(piece: ChessPiece, board: ChessBoard)->u64{
+    return ((piece.pos>>1)|(piece.pos>>9)|(piece.pos>>8)|(piece.pos>>7)|(piece.pos<<1)|(piece.pos<<9)|(piece.pos<<8)|(piece.pos<<7))&(!(get_piece_map(piece.colour,board)|get_capture_map_king_check(get_op_col(piece.colour), board)));
+}
+
+fn get_capture_map(col:ChessColour,board:ChessBoard)->u64{
+    let mut out:u64=0x00;
+    for piece in board.pieces{
+        if piece.colour!=col{continue;}
+        out=out|match piece.kind{
+            ChessPieceKind::Rook=>get_rook_moves(piece, board),
+            ChessPieceKind::Pawn=>get_pawn_captures(piece, board),
+            ChessPieceKind::Knight=>get_knight_moves(piece, board),
+            ChessPieceKind::King=>get_king_moves(piece,board),
+            ChessPieceKind::Bishop=>get_bishop_moves(piece, board),
+            ChessPieceKind::Queen=>get_queen_moves(piece, board),
         }
-        scanner=scanner>>1;
     }
-    return 0;
+    return out;
 }
+fn is_checked(col:ChessColour,board:ChessBoard)->bool{
+    let check_map=get_capture_map_king_check(get_op_col(col), board);
+    for piece in board.pieces{
+        if piece.colour!=col{continue;}
+        if piece.kind!=ChessPieceKind::King{continue;}
+        return (piece.pos&check_map)>0;
+    }
+    return true;
+}
+fn get_long_castle_move(piece:ChessPiece, board:ChessBoard)->bool{
+    if piece.has_moved{return false;}
+    if is_checked(piece.colour,board){return false;}
+    if piece.kind!=ChessPieceKind::King{return false}
+    if piece.colour==ChessColour::White{
+        let mut other_o:Option<ChessPiece>=get_piece_bit_mask(piece.pos<<4, board);
+        if other_o.is_none(){return false;}
+        let other:ChessPiece= other_o.take().unwrap();
+        if other.has_moved{return false;}
+        if other.kind!=ChessPieceKind::Rook{return false;}
+        let block_check_map=!(get_capture_map(get_op_col(piece.colour), board)|get_piece_map(piece.colour,board));
+        return ((((((piece.pos<<1)&block_check_map)<<1)&block_check_map)<<1)&block_check_map)>0;        
+        
+        
+    }
+    if piece.colour==ChessColour::Black{
+        let mut other_o:Option<ChessPiece>=get_piece_bit_mask(piece.pos>>4, board);
+        if other_o.is_none(){return false;}
+        let other:ChessPiece= other_o.take().unwrap();
+        if other.has_moved{return false;}
+        if other.kind!=ChessPieceKind::Rook{return false;}
+        let block_check_map=!(get_capture_map(get_op_col(piece.colour), board)|get_piece_map(piece.colour,board));
+        return ((((((piece.pos>>1)&block_check_map)>>1)&block_check_map)>>1)&block_check_map)>0;        
+    
+    }
+    return false;
+}
+
+
 
 fn get_colour_hash(col:ChessColour)->u8{
     return match col {
